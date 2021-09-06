e033e756e6e398c6bf29b5f6da5359f522952a10
Gunasekaran-Annamalai <gunasekaran021098@gmail.com>
2021-09-06 11:24:27 +0530

    After Security

--- virtual_hospital/settings.py
+++ virtual_hospital/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6wgvyqc_a$md+!979yy_$d03tber!c8xfs$a!n4e%5(2@y5br&'
+#####..... Secret key comes here .....##### 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
